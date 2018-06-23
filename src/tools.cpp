879988982a257fc97aee0dd9bb4e970b926e2b81
Nitheesh K L <nitheesh.k.l@intel.com>
2018-06-23 15:41:30 +0530

    calculate rmse
    Signed-off-by: Nitheesh K L <nitheesh.k.l@intel.com>

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,25 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  // check for invalid size
+  if ((estimations.size() == 0)
+        || (estimations.size() != ground_truth.size())) {
+      return rmse;
+  }
+
+  // sum of squares
+  for (unsigned int i = 0; i < estimations.size(); i++) {
+    VectorXd diff = estimations[i] - ground_truth[i];
+    diff = diff.array() * diff.array();
+    rmse += diff;
+  }
+  // mean
+  rmse /= estimations.size();
+  // sqrt of mean
+  rmse = rmse.array().sqrt();
+
+  return rmse;
 }
